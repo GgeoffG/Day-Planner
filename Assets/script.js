bbe4f62ead07a2e890e3ea8f56eb18daff131495
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-02-27 20:24:53 -0800

    change back

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,14 @@
-var currentDay = document.getElementById('currentDay')
+var currentDay=document.getElementById('currentDay')
 var ele = document.getElementsByClassName('time-block');
 console.log(currentDay)
-var idArray = []
+var idArray=[]
 
-for (var i = 0; i < ele.length; i++) {
+for (var i=0; i< ele.length; i++ ) {
   idArray.push(ele[i].id);
 }
-var buttons = document.querySelectorAll('.btn')
-var buttonArray = []
-for (i = 0; i < buttons.length; i++) {
+var buttons=document.querySelectorAll('.btn')
+var buttonArray= []
+for( i=0; i<buttons.length; i++){
   buttonArray.push(buttons[i].parentElement.id)
 }
 console.log(buttons)
@@ -18,46 +18,46 @@ console.log(buttonArray)
 
 
 $(function () {
-  function displayDay() {
-    today = dayjs().format('[Today is: ]MMM, DD')
-    currentDay.textContent = today
+  function displayDay(){
+    today=dayjs().format('[Today is: ]MMM, DD')
+    currentDay.textContent=today
   }
-  function timeCheck() {
-    for (i = 0; i < idArray.length; i++) {
-      let timeBlocks = document.getElementById(idArray[i])
-      let timeInt = parseInt(timeBlocks.id)
-      if (timeInt == dayjs().format('H'))
-        timeBlocks.classList.add('present')
-      if (timeInt < dayjs().format('H'))
-        timeBlocks.classList.add('past')
-      if (timeInt > dayjs().format('H'))
-        timeBlocks.classList.add('future')
-
+  function timeCheck(){
+    for (i=0; i<idArray.length;i++){
+      let timeBlocks=document.getElementById(idArray[i])
+      let timeInt=parseInt(timeBlocks.id)
+      if(timeInt == dayjs().format('H'))
+      timeBlocks.classList.add('present')
+      if(timeInt < dayjs().format('H'))
+      timeBlocks.classList.add('past')
+      if(timeInt > dayjs().format('H'))
+      timeBlocks.classList.add('future')
+      
     }
   }
-
-
+  
+    
   $(".time-block button").on("click", function (event) {
-    let textInput = $(this).siblings('.description').val();
-    let blockId = $(this).parent('.time-block').attr('id')
-    console.log(textInput);
-    localStorage.setItem(blockId, textInput);
-
-  })
-
-  function displaySavedTasks() {
-    for (i = 0; i < idArray.length; i++) {
-      tasks = document.getElementById(idArray[i])
-      savedTasks = localStorage.getItem(idArray[i])
-      if (localStorage.hasOwnProperty(idArray[i])) {
+      let textInput= $(this).siblings('.description').val();
+      let blockId= $(this).parent('.time-block').attr('id')
+      console.log(textInput);
+      localStorage.setItem(blockId,textInput);
+  
+    })
+  
+    function displaySavedTasks(){
+      for(i=0 ;i<idArray.length;i++){
+        tasks= document.getElementById(idArray[i])
+      savedTasks= localStorage.getItem(idArray[i])
+      if (localStorage.hasOwnProperty(idArray[i])){
         $('.time-block').eq(i).children('.description').text(savedTasks)
         console.log(savedTasks)
       }
-
+      
+      }
     }
-  }
 
   timeCheck()
-  displayDay()
-  displaySavedTasks()
+displayDay()
+displaySavedTasks()
 });
